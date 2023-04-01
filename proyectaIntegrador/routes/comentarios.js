59f133ad0bde6c62a19c9e64a0dc1919706d03e9
tomasharten <thartenstein@udesa.edu.ar>
2023-04-01 14:00:05 -0300

    linkee todas las rutas hasta ahora con sus controladores. Ya andan con nodemon.

--- proyectaIntegrador/routes/comentarios.js
+++ proyectaIntegrador/routes/comentarios.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 
 const comentariosController = require("../controllers/comentariosController");
-
+router.get ("/",comentariosController.estamos)
 module.exports= router
--- proyectaIntegrador/routes/search.js
+++ proyectaIntegrador/routes/search.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 
 const searchController = require("../controllers/searchController");
-
+router.get ("/",searchController.estamos)
 module.exports= router
