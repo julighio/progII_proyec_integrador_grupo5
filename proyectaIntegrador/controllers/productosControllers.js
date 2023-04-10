53cee9746ca7ad1a42d0a4d84a6dacf487080493
julighio <jghiorivas@udesa.edu.ar>
2023-04-10 16:45:55 -0300

    ...

--- proyectaIntegrador/controllers/productosControllers.js
+++ proyectaIntegrador/controllers/productosControllers.js
@@ -4,7 +4,10 @@ let data = require ('../db/data')
 
 const controladorProductos = {
     product: function(req,res){
-        return res.render ('product')
+        console.log(data.products);
+        return res.render ('product',{
+            product: data.products
+        })
     },
 
     productAdd: function(req,res){
