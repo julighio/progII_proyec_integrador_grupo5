0fc541d372f15cbcb21c5607f6e0a83fcad83d12
julighio <jghiorivas@udesa.edu.ar>
2023-06-07 17:02:31 -0300

    ..

--- proyectaIntegrador/database/config/config.js
+++ proyectaIntegrador/database/config/config.js
@@ -2,7 +2,7 @@ module.exports= {
   "development": {
     "username": "root",
     "password": null,
-    "database": "tp_integrador", //Aca pasar el nombre de la database ni idea vinilos
+    "database": "tp_integrador",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
